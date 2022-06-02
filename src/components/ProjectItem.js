918ef9b28653fced5b932625831f311ce359c1fa
hector <hpulido1021@gmail.com>
2022-06-01 22:14:47 -0700

    Completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let index = 0
+  const projects = technologies.map((element) => { 
+    index++
+   return <span key={index}>{element}</span>;
+  })
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+       {projects}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,13 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map((obj) => {
+    return <ProjectItem key ={obj.id} technologies = {obj.technologies} name={obj.name} about={obj.about}></ProjectItem>
+  })}</div>
     </div>
   );
 }
